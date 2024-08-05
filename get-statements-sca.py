bbf2af727998953bc572ff3d2d23b28984babc1f
Michal <mkurdziel1987@gmail.com>
2024-08-05 14:12:55 +0200

    save to file

--- get-statements-sca.py
+++ get-statements-sca.py
@@ -7,8 +7,8 @@ import urllib3
 from urllib.parse import urlencode
 from datetime import datetime, timedelta, timezone
 
-private_key_path = '/Users/michal/.ssh/wise.pem'  # Change to private key path
-profile_id = '26471881'                     # Change to profile ID
+private_key_path = '/Users/michal/.ssh/wise_rsa.pem'  # Change to private key path
+profile_id = '25984956'                     # Change to profile ID
 balance_id = '26471881'                     # Change to borderless account ID
 base_url = 'https://api.transferwise.com'
 
@@ -24,41 +24,45 @@ elif os.path.exists(private_key_path) is False:
 
 token = os.getenv('API_TOKEN')  # Set API token from env
 http = urllib3.PoolManager()
+format = 'xml'
+interval_start = (datetime.now(timezone.utc) - timedelta(days=14)).isoformat()
+interval_end = datetime.now(timezone.utc).isoformat()
 
 def get_statement(one_time_token, signature):
-    interval_start = (datetime.now(timezone.utc) - timedelta(days=14)).isoformat()
-    interval_end = datetime.now(timezone.utc).isoformat()
 
     params = urlencode({
-        'currency': 'GBP', 'type': 'COMPACT',
+        'currency': 'EUR', 'type': 'FLAT',
         'intervalStart': interval_start,
         'intervalEnd': interval_end})
 
     url = (
         base_url + '/v1/profiles/' + profile_id + '/balance-statements/' 
-        + balance_id + '/statement.json?' + params)
+        + balance_id + '/statement.'+format+'?' + params)
 
     headers = {
         'Authorization': 'Bearer ' + token,
         'User-Agent': 'tw-statements-sca',
-        'Content-Type': 'application/json'}
+        # 'Content-Type': 'application/'
+        }
     if one_time_token != "":
         headers['x-2fa-approval'] = one_time_token
         headers['X-Signature'] = signature
         print(headers['x-2fa-approval'], headers['X-Signature'])
 
     print('GET', url)
+    print('HEADERS', headers)
 
     r = http.request('GET', url, headers=headers, retries=False)
 
     print('status:', r.status)
     
     if r.status == 200 or r.status == 201:
-        return json.loads(r.data)
+        return r
     elif r.status == 403 and r.headers.get('x-2fa-approval') is not None:
         one_time_token = r.headers.get('x-2fa-approval')
         signature = do_sca_challenge(one_time_token)
-        get_statement(one_time_token, signature)
+        response = get_statement(one_time_token, signature)
+        return response
     else:
         print('failed: ', r.status)
         print(r.data)
@@ -87,22 +91,29 @@ def do_sca_challenge(one_time_token):
     return signature
 
 def main():
-    statement = get_statement("", "")
+    r = get_statement("", "")
+    content_type = r.headers.get('Content-Type')
 
-    if statement is not None and 'currency' in statement['request']:
-        currency = statement['request']['currency']
-    else:
-        print('something is wrong')
-        print(statement)
-        sys.exit(0)
-    
-    if 'transactions' in statement:
-        txns = len(statement['transactions'])
-    else:
-        print('Empty statement')
-        sys.exit(0)
+    print(r.headers.get('Content-Type'))
 
-    print('\n', currency, 'statement received with', txns, 'transactions.')
+    # if content_type is 'application/json' and r.data is not None and 'currency' in r.data['request']:
+    #     currency = r.data['request']['currency']
+    # elif content_type in ['application/pdf']:
+    #     with open('wise.pdf', 'wb') as f:
+    #         f.write(r.data)
+    # else:
+    #     print('something is wrong')
+    #     print(r.data)
+    #     sys.exit(0)
+    with open('wise-statement-'+interval_start[:10]+'-'+interval_end[:10]+'.'+ format, 'wb') as f:
+        f.write(r.data)   
+    # if 'transactions' in r.data:
+    #     txns = len(r.data['transactions'])
+    # else:
+    #     print('Empty statement')
+    #     sys.exit(0)
+
+    # print('\n', currency, 'statement received with', txns, 'transactions.')
 
 if __name__ == '__main__':
     main()
